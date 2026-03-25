9d353556843248c2278321476407172f42a1e57b
0xhighstandards <0xhighstandards@gmail.com>
2026-03-25 12:37:26 +0800

    update more

--- sofe_api/app.py
+++ sofe_api/app.py
@@ -106,7 +106,7 @@ Input:
                     {"role": "user", "content": ai_prompt}
                 ]
             },
-            timeout=(10, 90)
+            timeout=(10, 600)
         )
 
         if response.status_code != 200:
